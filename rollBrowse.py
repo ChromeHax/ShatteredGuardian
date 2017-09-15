d21b2f51d20226a406bc6bfd840f3d999d285545
ChromeHax <31979801+ChromeHax@users.noreply.github.com>
2017-09-14 22:31:19 -0700

    rollBrowse created
    rollBrowse is an extremely simple script which allows you to use a browser sandbox on RollApp, even under a free account. Simply run it from Wing IDE (In the Development category). Please note that the virtual filesystem is still read-only, so you cannot download files or plugins.

---
+++ rollBrowse.py
@@ -0,0 +1,6 @@
+# RollApp Browser bypass - 9/14/2017
+# Used to spawn a browser in a free session using Wing IDE
+# Very simple script - simply copy and paste into an empty project in Wing on RollApp, then run it. Kachow! Instant browser for free.
+
+import webbrowser
+webbrowser.open_new("google.com")
